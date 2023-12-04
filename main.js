e6989252d323ecd271b9f3136fe7b2d05e1cc5c6
wodus <wodustjdus34@gmail.com>
2023-12-04 22:57:56 +0900

    [Add] 재연 music 페이지 라우팅

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ app.set("views", path.join(__dirname, "views"));
 //body-parser 추가
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); // Express 미들웨어 설정
+
+// Route.js 파일을 가져와서 사용
+const musicRoute = require("./routes/musicRoute");
+
+// Route를 등록
+app.use("/music", musicRoute);
+
+// 서버 시작
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
